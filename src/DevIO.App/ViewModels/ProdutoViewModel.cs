e9d3fb40ab74b67d2cf20c77bcdee9b2a42e650b
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-04-25 14:14:23 -0300

    Globalizando a aplicação em pt-BR

--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DevIO.App.Extensions;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -31,6 +32,7 @@ namespace DevIO.App.ViewModels
 
         public string Imagem { get; set; }
 
+        [Moeda]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
         
