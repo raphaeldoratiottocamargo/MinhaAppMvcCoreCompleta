b0977d52a0102604d13470e136ab24ec2aed40b9
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-04-25 16:53:03 -0300

    Finalizando o ajuste das Views

---
+++ src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatarDocumento(this RazorPage page, int tipoPessoa, string Documento)
+        {
+            return tipoPessoa == 1 ?
+                Convert.ToUInt64(Documento).ToString(@"000\.000\.000\-00") :
+                Convert.ToUInt64(Documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+    }
+}
