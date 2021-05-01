5a8bc29400c79fadceb456bf29618dcb5021a5c4
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-05-01 20:42:28 -0300

    Validando as entidades de negócio

---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    /// <summary>
+    /// Apenas os serviços que alteram o estado do Fornecedor
+    /// </summary>
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if(!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DevIO.Business/Services/ProdutoService.cs
@@ -0,0 +1,29 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    /// <summary>
+    /// Apenas os serviços que alteram o estado do Produto
+    /// </summary>
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
