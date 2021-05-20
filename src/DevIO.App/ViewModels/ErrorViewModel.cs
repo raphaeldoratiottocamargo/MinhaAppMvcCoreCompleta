ea05f453a83510dfaa9ae9224f3a25cdff836f6a
raphaeldoratiottocamargo <raphaeldoratiottocamargo@gmail.com>
2021-05-20 17:43:23 -0300

    Tratamento de Erros

--- src/DevIO.App/ViewModels/ErrorViewModel.cs
+++ src/DevIO.App/ViewModels/ErrorViewModel.cs
@@ -1,11 +1,11 @@
-using System;
-
 namespace DevIO.App.ViewModels
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public int ErrorCode { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Titulo { get; set; }
+
+        public string Mensagem { get; set; }
     }
 }
